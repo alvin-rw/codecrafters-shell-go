b6e099cee3e82e028a86d0587f8dfa1c090e7841
Alvin Wonys <alvin.rw@proton.me>
2024-06-18 21:33:25 +0800

    REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,15 +8,17 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Printf("error when reading command %s", err)
-		os.Exit(1)
+		// Wait for user input
+		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Printf("error when reading command %s", err)
+			os.Exit(1)
+		}
+		cmd = strings.TrimSuffix(cmd, "\n")
+
+		fmt.Printf("%s: command not found\n", cmd)
 	}
-	cmd = strings.TrimSuffix(cmd, "\n")
-
-	fmt.Printf("%s: command not found\n", cmd)
 }
