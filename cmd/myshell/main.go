670b11a71211546bd6b6f028769759f92a31a26e
Alvin Wonys <alvin.rw@proton.me>
2024-06-18 21:35:46 +0800

    The exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,21 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	rd := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		// Wait for user input
-		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		cmd, args, err := readInput(rd)
 		if err != nil {
 			fmt.Printf("error when reading command %s", err)
 			os.Exit(1)
 		}
-		cmd = strings.TrimSuffix(cmd, "\n")
 
-		fmt.Printf("%s: command not found\n", cmd)
+		switch cmd {
+		case "exit":
+			exit(args[0])
+		default:
+			fmt.Printf("%s: command not found\n", cmd)
+		}
 	}
+}
+
+func readInput(rd *bufio.Reader) (string, []string, error) {
+	userInput, err := rd.ReadString('\n')
+	if err != nil {
+		return "", nil, err
+	}
+
+	userInput = strings.TrimSuffix(userInput, "\n")
+	userInputSlice := strings.Split(userInput, " ")
+
+	command := userInputSlice[0]
+
+	var args []string
+	if len(userInputSlice) > 1 {
+		args = userInputSlice[1:]
+	}
+
+	return command, args, nil
+}
+
+func exit(input string) error {
+	statusCode, err := strconv.Atoi(input)
+	if err != nil {
+		return err
+	}
+
+	os.Exit(statusCode)
+	return nil
 }
